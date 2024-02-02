2379a3acba31cdc95b78892a82d24e05af15e837
shooore <shooore@gmail.com>
2024-02-02 18:01:27 +0300

    ...

--- site/coral.ru/components/ProduitVue.vue
+++ site/coral.ru/components/ProduitVue.vue
@@ -1,12 +1,20 @@
 <script setup>
 
+import { ref } from "vue";
+
 const props = defineProps(['options', 'productList']);
+
+const selected = ref();
+const options = ref(['opt1', 'opt2', 'opt3']);
 
 </script>
 
 <template>
     <div>
         {{ props.options.prop }}
+        <Dropdown v-model="selected" :options="options" unstyled>
+            <template #header>HEADER</template>
+        </Dropdown>
     </div>
 </template>
 
--- site/coral.ru/js/init1.js
+++ site/coral.ru/js/init1.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import ProduitVue from "../components/ProduitVue.vue";
 
-
 createApp(ProduitVue, {
     options: {
         prop: 'list 1'
     }
 })
+    .use(primevue.config.default)
+    .component('Dropdown', primevue.dropdown)
     .mount('[data-produit-vue="produit-vue-1"]');
