ae3bd1a4827466cfdee92540e54b08f18d8cef8c
shooore <shooore@gmail.com>
2024-02-06 10:12:16 +0300

    reworked with CDN based elemet-plus

--- site/coral.ru/components/ProduitVue.vue
+++ site/coral.ru/components/ProduitVue.vue
@@ -1,30 +1,68 @@
 <script setup>
 import departures from '../config/departures.yaml';
-import Dropdown from "primevue/dropdown";
 
 import { ref } from "vue";
 
 const props = defineProps(['options', 'productList']);
 
-const selectedDeparture = ref(departures[0]);
-const departureOptions = ref(departures);
-
-const date = ref();
-
 </script>
 
 <template>
-    <div>
-        {{ props.options.prop }}
-        <Dropdown v-model="selectedDeparture" filter
-                  :options="departureOptions" option-label="name">
-            <template #value="{ value }">из {{ value.fromForm }}</template>
-            <template #header>город вылета</template>
-            <template #option="{ option }">{{ option.correctName ?? option.name }}</template>
-        </Dropdown>
+    <div class="produit-vue">
+        <div class="controls">
+            controls
+        </div>
+        <div class="product-grid">
+            list
+        </div>
     </div>
 </template>
 
-<style scoped>
+<style lang="less">
+@import "../common/css/coral-fonts";
+@import "../common/css/coral-colors";
+
+:root {
+    --el-font-family: 'museosans'!important;
+    //--el-font-size-base: inherit!important;
+    --el-font-size-base: 1em!important;
+    --el-component-size: 2.5em!important;
+    --el-fill-color-light: fade(@coral-main-blue, 8%)!important;
+}
+
+.el-select-dropdown {
+    font-family: museosans;
+    font-weight: 400;
+    --el-color-primary: @coral-main-blue;
+    //--el-text-color-regular: black;
+}
+
+.tour-guide-module {
+    .el-select {
+        --el-select-width: unset;
+        --el-select-border-color-hover: @coral-main-blue;
+        .el-input__wrapper {
+            --el-input-border-color: @coral-page-bg;
+            box-shadow: inset 0 0 0 2px var(--el-input-border-color);
+        }
+        .el-select__wrapper {
+            font-size: unset;
+        }
+    }
+}
+
+</style>
+
+<style scoped lang="less">
+@import "../common/css/layout";
+
+.produit-vue {
+    .bbox();
+    .silly-b2c-font-size(@max-font-size: 14px);
+    font-family: museosans;
+    font-weight: normal;
+    display: grid;
+    gap: 1em;
+}
 
 </style>
--- site/coral.ru/js/init1.js
+++ site/coral.ru/js/init1.js
@@ -1,14 +1,18 @@
 import { createApp } from "vue";
 import ProduitVue from "../components/ProduitVue.vue";
-import PrimeVue from "primevue/config";
+import ElementPlus from 'element-plus';
+
+const app_instance_id = 'produit-vue-1';
+const app_el = document.querySelector(`[data-produit-vue="${ app_instance_id }"]`);
+
+app_el
+    .closest('.oti-content-typography')
+    .classList.remove('oti-content-typography');
 
 createApp(ProduitVue, {
     options: {
         prop: 'list 1'
     }
 })
-    .use(PrimeVue)
-    // .component('Dropdown', primevue.dropdown)
-    // .component('MultiSelect', primevue.multiselect)
-    // .component('Calendar', primevue.calendar)
-    .mount('[data-produit-vue="produit-vue-1"]');
+    .use(ElementPlus)
+    .mount(app_el);
